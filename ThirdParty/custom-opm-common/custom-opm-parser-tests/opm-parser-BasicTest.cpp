da87babc8eb06f724ecf265415a6a7028a497cb5
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2020-03-23 16:04:42 +0100

    Improved test

--- ThirdParty/custom-opm-common/custom-opm-parser-tests/opm-parser-BasicTest.cpp
+++ ThirdParty/custom-opm-common/custom-opm-parser-tests/opm-parser-BasicTest.cpp
@@ -2,6 +2,8 @@
 #include "gtest/gtest.h"
 
 #include "opm/parser/eclipse/Parser/Parser.hpp"
+#include "opm/parser/eclipse/EclipseState/Schedule/VFPInjTable.hpp"
+#include "opm/parser/eclipse/EclipseState/Schedule/VFPProdTable.hpp"
 
 
 using namespace Opm;
@@ -13,20 +15,49 @@ using namespace Opm;
 TEST(OpmParserTest, ReadFromFile)
 {
     ParseContext parseContext;
-    Parser parser;
 
-    //std::string testFile = "d:/gitroot-ceesol/ResInsight-regression-test/ModelData/norne/NORNE_ATW2013.DATA";
-    std::string testFile = "d:/gitroot-ceesol/ResInsight-regression-test/ModelData/norne/INCLUDE/VFP/B1BH.Ecl";
-
-    auto deck = parser.parseFile(testFile);
-
-    std::string myKeyword = "VFPPROD";
-    auto keywordList = deck.getKeywordList(myKeyword);
-
-    for (auto kw : keywordList)
     {
-        auto name = kw->name();
+        Parser parser;
+
+        std::string testFile = "d:/gitroot-ceesol/ResInsight-regression-test/ModelData/norne/INCLUDE/VFP/B1BH.Ecl";
+
+        auto deck = parser.parseFile(testFile);
+
+        std::string myKeyword = "VFPPROD";
+        auto keywordList = deck.getKeywordList(myKeyword);
+
+        UnitSystem unitSystem;
+
+    
+        for (auto kw : keywordList)
+        {
+            auto name = kw->name();
+    
+            VFPProdTable table(*kw, unitSystem);
+            std::cout << table.getDatumDepth() << std::endl;
+        }
     }
+    {
+        Parser parser;
+
+        std::string testFile = "d:/gitroot-ceesol/ResInsight-regression-test/ModelData/norne/INCLUDE/VFP/C1H.Ecl";
+
+        auto deck = parser.parseFile(testFile);
+
+        std::string myKeyword = "VFPINJ";
+        auto keywordList = deck.getKeywordList(myKeyword);
+
+        UnitSystem unitSystem;
+    
+        for (auto kw : keywordList)
+        {
+            auto name = kw->name();
+    
+            VFPInjTable table(*kw, unitSystem);
+            std::cout << table.getDatumDepth() << std::endl;
+        }
+    }
+
 
 }
 
