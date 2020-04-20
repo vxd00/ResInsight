263beb8cf6a9f84a0a68ae50456eb8af2d44053b
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2020-04-20 09:48:03 +0200

    #5773 Read VFPInjTable using Opm::Parser.

--- ApplicationCode/ProjectDataModel/CMakeLists_files.cmake
+++ ApplicationCode/ProjectDataModel/CMakeLists_files.cmake
@@ -153,6 +153,7 @@ ${CMAKE_CURRENT_LIST_DIR}/RimWellMeasurementFilter.h
 ${CMAKE_CURRENT_LIST_DIR}/RimWellMeasurementInViewCollection.h
 ${CMAKE_CURRENT_LIST_DIR}/RimWellMeasurementInView.h
 ${CMAKE_CURRENT_LIST_DIR}/RimWellDiskConfig.h
+${CMAKE_CURRENT_LIST_DIR}/RimVfpTableExtractor.h
 )
 
 
@@ -310,6 +311,7 @@ ${CMAKE_CURRENT_LIST_DIR}/RimWellMeasurementFilter.cpp
 ${CMAKE_CURRENT_LIST_DIR}/RimWellMeasurementInViewCollection.cpp
 ${CMAKE_CURRENT_LIST_DIR}/RimWellMeasurementInView.cpp
 ${CMAKE_CURRENT_LIST_DIR}/RimWellDiskConfig.cpp
+${CMAKE_CURRENT_LIST_DIR}/RimVfpTableExtractor.cpp
 )
 
 list(APPEND CODE_HEADER_FILES
